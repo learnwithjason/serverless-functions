9dcfdec3e56191a657c62cf35c52ad6add7f7a93
Jason Lengstorf <jason@lengstorf.com>
2020-11-28 18:55:53 -0800

    feat: working app!

--- functions/load-corgis.js
+++ functions/load-corgis.js
@@ -1,4 +1,5 @@
 const fetch = require('node-fetch');
+const { hasuraRequest } = require('./util/hasura');
 
 exports.handler = async () => {
   const corgis = await fetch(
@@ -14,16 +15,37 @@ exports.handler = async () => {
     }
   ).then((res) => res.json());
 
-  const [unsplashData] = await Promise.all([unsplashPromise]);
+  const hasuraPromise = hasuraRequest({
+    query: `
+      mutation InsertOrUpdateBoops($corgis: [boops_insert_input!]!) {
+        boops: insert_boops(objects: $corgis, on_conflict: {constraint: boops_pkey, update_columns: id}) {
+          returning {
+            id
+            count
+          }
+        }
+      }
+    `,
+    variables: {
+      corgis: corgis.map(({ id }) => ({ id, count: 0 })),
+    },
+  });
+
+  const [unsplashData, hasuraData] = await Promise.all([
+    unsplashPromise,
+    hasuraPromise,
+  ]);
 
   const completeData = corgis.map((corgi) => {
     const photo = unsplashData.find((p) => corgi.id === p.id);
+    const boops = hasuraData.boops.returning.find((b) => b.id === corgi.id);
 
     return {
       ...corgi,
       alt: photo.alt_description,
       credit: photo.user.name,
       url: `${photo.urls.raw}&auto=format&fit=crop&w=300&h=300&q=80&crop=entropy`,
+      boops: boops.count,
     };
   });
 
